4bc8c913e8f0c68c2d39ce11c2c6219bc604a1a7
AlanHorowitz <alanhorowitz63@gmail.com>
2021-07-02 10:38:27 -0400

    Get it to work with standalone executor

--- dags/market_vol.py
+++ dags/market_vol.py
@@ -11,7 +11,7 @@ work_dir = os.getcwd()
 temp_dir = '/tmp/data'
 
 default_args = {
-    'start_date': datetime(2020, 1, 1)
+    'start_date': datetime(2021, 6, 25)
 }
 
 
@@ -19,12 +19,12 @@ def create_report(report_date):
     pass
 
 
-def yahoo_finance_data(symbol, report_date):
+def yahoo_finance_data(symbol, report_date, out_dir):
     out_filename = symbol + '.csv'
     start_date = date.fromisoformat(report_date)
-    end_date = start_date - timedelta(days=1)
+    end_date = start_date + timedelta(days=1)
     df = yf.download(symbol, start=start_date, end=end_date, interval='1m')
-    df.to_csv(os.path.join(work_dir, out_filename), header=False)
+    df.to_csv(os.path.join(out_dir, report_date, out_filename), header=False)
 
 
 with DAG('market_vol',
@@ -35,21 +35,26 @@ with DAG('market_vol',
 
     t0 = BashOperator(task_id='create_temp_dir',
                       bash_command='mkdir -p {{params.temp_dir}}/{{ ds }}',
-                      params={'temp_dir': f'{temp_dir}'})
+                      params={'temp_dir': temp_dir})
 
     t1 = PythonOperator(task_id='download_AAPL',
                         python_callable=yahoo_finance_data,
-                        op_kwargs={'symbol': 'AAPL', 'report_date': '{{ ds }}'})
+                        op_kwargs={'symbol': 'AAPL', 'report_date': '{{ ds }}', 'out_dir': temp_dir})
 
     t2 = PythonOperator(task_id='download_TSLA',
                         python_callable=yahoo_finance_data,
-                        op_args=['TSLA'])
+                        op_kwargs={'symbol': 'TSLA', 'report_date': '{{ ds }}', 'out_dir': temp_dir})
 
-    t3 = BashOperator(task_id='move_AAPL_to_data_location',
-                      bash_command=f'mv {work_dir}/AAPL.csv {temp_dir}/2021-07-01/')
+    t3 = BashOperator(task_id='move_AAPL',
+                      bash_command='mv {{params.temp_dir}}/{{ ds }}/AAPL.csv {{params.work_dir}}',
+                      params={'temp_dir': temp_dir, 'work_dir': work_dir})
 
-    t4 = BashOperator(task_id='move_TSLA_to_data_location',
-                      bash_command=f'mv {work_dir}/TSLA.csv {temp_dir}/2021-07-01/')
+    t4 = BashOperator(task_id='move_TSLA',
+                      bash_command='mv {{params.temp_dir}}/{{ ds }}/TSLA.csv {{params.work_dir}}',
+                      params={'temp_dir': temp_dir, 'work_dir': work_dir})
 
     t5 = PythonOperator(task_id='daily_report',
                         python_callable=create_report)
+                        op_kwargs={'report_date': '{{ ds }}'}
+
+    t0 >> t1 >> t2 >> t3 >> t4 >> t5
